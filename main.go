da4380aa1d81f3c7813117d016e1968ecadb8489
Author Name <fcorrionero@gmail.com>
2021-03-10 21:29:54 +0100

    Refactors dishes_controller

--- main.go
+++ main.go
@@ -7,9 +7,7 @@ import (
 func main() {
 	repo := InitializeDishesRepository()
 	dishesController := InitializeDishesHttpController(repo)
-	http.HandleFunc("/name", dishesController.ByName)
-	http.HandleFunc("/id", dishesController.ById)
-	http.HandleFunc("/allergen", dishesController.ByAllergen)
+	http.HandleFunc("/search", dishesController.Search)
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		panic(err)
